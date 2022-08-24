49bb4aaf50068081859cf4792f2b5363b7d0073b
Prakash-droid <54974293+Prakash-droid@users.noreply.github.com>
2022-08-24 13:53:04 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication extends SpringBootServletInitializer {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello Azure vg!";
+		return "Hello Azure VANGUARD!";
 	}
 }
